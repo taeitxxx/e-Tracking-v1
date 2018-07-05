51295a8527a2c85c87be4fe8050ed9730c62252d
Panut <panut@protos.co.th>
2018-07-05 15:47:11 +0700

    Add Plan Cost calculator and master mockup to project

---
+++ e-Tracking/Controllers/PlanCostController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using e_Tracking.Models;
+
+namespace e_Tracking.Controllers
+{
+    public class PlanCostController : Controller
+    {
+        // GET: PlanCost
+        public ActionResult Index()
+        {
+            PlanCostViewModel model = new PlanCostViewModel();
+
+            IEnumerable<SelectListItem> currencies = CultureInfo.GetCultures(CultureTypes.SpecificCultures)
+                .Select(ci => ci.LCID).Distinct()
+                .Select(id => new RegionInfo(id))
+                .GroupBy(r => r.ISOCurrencySymbol)
+                .Select(g => g.First())
+                .Select(r => new SelectListItem
+                {
+                    Text = r.ISOCurrencySymbol,
+                    Value = r.ISOCurrencySymbol
+                });
+
+            model.All_Currencies = currencies;
+
+            return View(model);
+        }
+        public ActionResult Master()
+        {
+            return View();
+        }
+    }
+}
---
+++ e-Tracking/Models/PlanCostViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace e_Tracking.Models
+{
+    public class PlanCostViewModel
+    {
+        public IEnumerable<SelectListItem> All_Currencies { get; set; }
+    }
+}
