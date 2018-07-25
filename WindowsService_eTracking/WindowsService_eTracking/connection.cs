535259d1d897c3085ccfe4868c59f07730d5f991
adevsan <41224456+adevsan@users.noreply.github.com>
2018-07-25 23:41:17 +0700

    Add files via upload

---
+++ WindowsService_eTracking/WindowsService_eTracking/connection.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService_eTracking
+{
+    class connection
+    {
+        public String getConnection_dev()
+        {
+            string strConnString = @"data source=DESKTOP-7PAQ8NK\MSSQLSERVER_ADEV;initial catalog=dbetracking;persist security info=True;user id=sa;password=admin50";
+
+            return strConnString;
+        }
+
+
+        public DataTable getUserFollow()
+        {
+            SqlConnection objConn = new SqlConnection();
+            SqlCommand objCmd = new SqlCommand();
+            SqlDataAdapter dtAdapter = new SqlDataAdapter();
+
+            DataSet ds = new DataSet();
+            DataTable dt = null;
+            string strSQL = null;
+
+            strSQL = " select * from tbfollowing where flag_follow = 'y'  ";
+
+            objConn.ConnectionString = getConnection_dev();
+            var _with1 = objCmd;
+            _with1.Connection = objConn;
+            _with1.CommandText = strSQL;
+            _with1.CommandType = CommandType.Text;
+            dtAdapter.SelectCommand = objCmd;
+
+            dtAdapter.Fill(ds);
+            dt = ds.Tables[0];
+
+            dtAdapter = null;
+            objConn.Close();
+            objConn = null;
+
+            return dt;
+        }
+
+
+        public DataTable getLastStatusByUserid(string userid, string pr_number)
+        {
+            SqlConnection objConn = new SqlConnection();
+            SqlCommand objCmd = new SqlCommand();
+            SqlDataAdapter dtAdapter = new SqlDataAdapter();
+
+            DataSet ds = new DataSet();
+            DataTable dt = null;
+            string strSQL = null;
+
+            strSQL = " select * from tblog_last_status where userid = '" + userid + "' and pr_number = '" + pr_number + "'  ";
+
+            objConn.ConnectionString = getConnection_dev();
+            var _with1 = objCmd;
+            _with1.Connection = objConn;
+            _with1.CommandText = strSQL;
+            _with1.CommandType = CommandType.Text;
+            dtAdapter.SelectCommand = objCmd;
+
+            dtAdapter.Fill(ds);
+            dt = ds.Tables[0];
+
+            dtAdapter = null;
+            objConn.Close();
+            objConn = null;
+
+            return dt;
+        }
+
+
+        public DataTable getLastStatus_PR(string pr_number)
+        {
+            SqlConnection objConn = new SqlConnection();
+            SqlCommand objCmd = new SqlCommand();
+            SqlDataAdapter dtAdapter = new SqlDataAdapter();
+
+            DataSet ds = new DataSet();
+            DataTable dt = null;
+            string strSQL = null;
+
+            strSQL = " select top(1) s.statusid, s.statusname, l.* from tblog l inner join tbstatus s on l.statusid = s.statusid " +
+                    " where pr_number = '" + pr_number + "' order by logId desc  ";
+
+            objConn.ConnectionString = getConnection_dev();
+            var _with1 = objCmd;
+            _with1.Connection = objConn;
+            _with1.CommandText = strSQL;
+            _with1.CommandType = CommandType.Text;
+            dtAdapter.SelectCommand = objCmd;
+
+            dtAdapter.Fill(ds);
+            dt = ds.Tables[0];
+
+            dtAdapter = null;
+            objConn.Close();
+            objConn = null;
+
+            return dt;
+        }
+
+
+        public void Insert_last_Status(string userid, string pr_number, string last_status, string lastmodifieBy)
+        {
+            SqlConnection objConn = new SqlConnection();
+            SqlCommand objCmd = new SqlCommand();
+            SqlDataAdapter dtAdapter = new SqlDataAdapter();
+
+            DataSet ds = new DataSet();
+            DataTable dt = null;
+            string strSQL = null;
+
+            strSQL = " Insert into tblog_last_status(userid,pr_number,last_status,lastmodifieddate,lastmodifieBy) " +
+                       " values('" + userid + "','" + pr_number + "','" + last_status + "',getdate(),'" + lastmodifieBy + "')  ";
+
+
+            objConn.ConnectionString = getConnection_dev();
+            objConn.Open();
+            var _with1 = objCmd;
+            _with1.Connection = objConn;
+            _with1.CommandText = strSQL;
+            _with1.CommandType = CommandType.Text;
+
+            objCmd.ExecuteNonQuery();
+
+            dtAdapter = null;
+            objConn.Close();
+            objConn = null;
+
+        }
+
+        public void Update_last_Status(string last_status, string userid, string pr_number)
+        {
+            SqlConnection objConn = new SqlConnection();
+            SqlCommand objCmd = new SqlCommand();
+            SqlDataAdapter dtAdapter = new SqlDataAdapter();
+
+            DataSet ds = new DataSet();
+            DataTable dt = null;
+            string strSQL = null;
+
+            strSQL = " update tblog_last_status set " +
+                    " last_status = '"+ last_status + "',lastmodifieddate = getdate() where userid = '"+ userid + "' and pr_number = '"+ pr_number + "' ";
+
+
+            objConn.ConnectionString = getConnection_dev();
+            objConn.Open();
+            var _with1 = objCmd;
+            _with1.Connection = objConn;
+            _with1.CommandText = strSQL;
+            _with1.CommandType = CommandType.Text;
+
+            objCmd.ExecuteNonQuery();
+
+            dtAdapter = null;
+            objConn.Close();
+            objConn = null;
+
+        }
+    }
+}
---
+++ WindowsService_eTracking/WindowsService_eTracking/sent_email.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsService_eTracking
+{
+    class sent_email
+    {
+        public void CallMail(string email, string touser, string subject, string Detail)
+        {
+            MailMessage Mail = new MailMessage();
+
+            Mail.To.Add(new MailAddress(email));
+            //Mail.[Bcc].Add(New MailAddress("pichayanee.rojrattanapon@daimler.com"))
+            //   Mail.Bcc.Add(new MailAddress("ajn_149@hotmail.com"));
+
+            Mail.From = new MailAddress("adevsolutionsplus@gmail.com"); // MailServer
+            Mail.Subject = "[e-Tracking] " + subject;
+
+            //'*** HTML Tag ***// 
+
+            StringBuilder BD = new StringBuilder();
+            BD.Append("<body bgcolor='#F8F7F1'>");
+            BD.Append("<div style='font-size:12px; width:550px; line-height:20px; " +
+                      "font-family:Tahoma, Geneva, sans-serif; border: 1px dashed #e1e1e1;background-color: #FFFFFF;padding: 0 20px;'>");
+            BD.Append("<p>Dear " + touser + "</p>");
+            BD.Append("<br/>");
+            BD.Append("<p>" + subject + "</p>");
+            BD.Append("<p>" + Detail + "</p>");
+            BD.Append("<br/>");
+            //BD.Append(@"<p>Please Check : http://103.40.142.138:8088/ </p>");
+            BD.Append("<br/>");
+            BD.Append("<p>Best Regards,</p>");
+            BD.Append("<br/>");
+            BD.Append("<p>e-Tracking</p>");
+
+            BD.Append("</div>");
+            BD.Append("</body>");
+
+            Mail.Body = BD.ToString();
+
+            try
+            {
+                Mail.IsBodyHtml = true;
+                SmtpClient client = new SmtpClient();
+                client.UseDefaultCredentials = true;
+                client.Host = "smtp.gmail.com";
+                client.Port = 587;
+                client.EnableSsl = true;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.Credentials = new NetworkCredential("adevsolutionsplus@gmail.com", "Admin2016");
+                client.Timeout = 20000;
+
+                client.Send(Mail);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
